f3bf5635bd16847984627910458ee7bcd560fa49
Gwilherm Couffy <90566016+Hellnettles@users.noreply.github.com>
2021-10-28 17:05:25 +0200

    Add files via upload

---
+++ Car.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once 'Vehicule.php';
+
+class Car extends Vehicle
+{
+
+    private string $energy;
+
+    private int $energyLevel ;
+
+    private bool $hasParkBrake;
+
+    public const ALLOWED_ENERGIES = [
+        'fuel',
+        'electric',
+    ];
+
+    public function __construct(string $color, int $nbSeats, string $energy) {
+        parent::__construct($color, $nbSeats);
+        $this->setEnergy($energy);
+    }
+
+
+    public function getEnergy(): string {
+        return $this->energy;
+    }
+
+    public function setEnergy(string $energy): Car
+    {
+        if (in_array($energy, self::ALLOWED_ENERGIES)) {
+            $this->energy = $energy;
+        }
+        return $this;
+    }
+
+    public function getEnergyLevel(): string {
+        return $this->energylevel;
+    }
+
+    public function setEnergyLevel(int $energyLevel): void
+    {
+        $this->energyLevel = $energyLevel;
+    }
+
+    public function start() {
+        
+        if ($this->hasParkBrake == true) {
+            throw new Exception("le frein a main est levé");
+        }
+        return "Démarrage du moteur, activation des phares et lancement du gps. </br>";
+    }
+
+    public function setParkBrake(bool $hasParkBrake) 
+    {
+        $this->hasParkBrake = $hasParkBrake;
+    }
+}
